746fcd6b7967f31909e81c532759b886f4454f8d
Floyd Schauer <floyd.schauer@me.com>
2014-05-21 15:48:43 +0200

    added gnuplot output

--- main_ising_1.cc
+++ main_ising_1.cc
@@ -9,15 +9,14 @@ using namespace std;
 
 #include "lattice.h"	// lattice class
 
-#include "../gnuplot-iostream/gnuplot-iostream.h" // gnuplot output
+#include "gnuplot_i.hpp" //Gnuplot class handles POSIX-Pipe-communikation with Gnuplot
 
-#include <boost/tuple/tuple.hpp>
-
-
+void wait_for_key(); // Programm halts until keypress
 
 int main(int argc, char *argv[])
 {	
-
+	
+	
 	if(argc != 7){
 		cout<<"usage : ising_ex <length> <dimension> <B-field> <iterations> <T> <eq_time>"<<endl;
 		return 1;
@@ -37,7 +36,37 @@ int main(int argc, char *argv[])
 
 	l1.run();
 	
+	try
+	{
+		Gnuplot g1("lines");
+		
+		g1.plot_x(l1.get_mag_vec(),"Magnetization per spin versus MC time");
+		
+		wait_for_key();		
+		}
+	catch (GnuplotException ge)
+    {
+        cout << ge.what() << endl;
+    }
+	
 	
 	return 0;
+}
+
+void wait_for_key ()
+{
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__TOS_WIN__)  // every keypress registered, also arrow keys
+    cout << endl << "Press any key to continue..." << endl;
+
+    FlushConsoleInputBuffer(GetStdHandle(STD_INPUT_HANDLE));
+    _getch();
+#elif defined(unix) || defined(__unix) || defined(__unix__) || defined(__APPLE__)
+    cout << endl << "Press ENTER to continue..." << endl;
+
+    std::cin.clear();
+    std::cin.ignore(std::cin.rdbuf()->in_avail());
+    std::cin.get();
+#endif
+    return;
 }
 
